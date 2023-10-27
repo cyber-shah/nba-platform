4bb3ed7ba24caadbea9a1ee109e042b3ea2b4895
cyber-shah <pranchx@gmail.com>
2023-10-26 21:36:22 -0400

    a home page and a login page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TextField from '@mui/material/TextField';
 
+// learning events
+// learning states
+// learning hooks
 export const Home = () => {
+    const handleUpClick = () => {
+        console.log("Uppercase was clicked")
+        setText("You have clicked on button")
+    }
+
+    const handleOnChange = (event) => {
+        console.log("On change")
+        setText(event.target.value)
+    }
+
+    const [text, setText] = useState('None');
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <>
+        <div style={{ display: 'grid', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
             <TextField
             id="outlined-multiline-static"
             label="Multiline"
             multiline
             rows={4}
-            defaultValue="Default Value"
+            value={text}
+            onChange={handleOnChange}
             />
         </div>
+
+        <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to Uppercase</button>
+
+        </>
     );
 };
