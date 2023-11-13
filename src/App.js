b0a740ba5df2797fe65488e64a5f6424f6c7305b
cyber-shah <pranchx@gmail.com>
2023-11-12 23:08:07 -0500

    after rebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { LoginPage } from './components/LoginPage';
 import { Home } from './components/Home';
+import{ ChartApp } from './components/Calender.tsx';
+import Example from './components/Example.tsx';
+import Radar from './components/Radar.tsx';
+import Rechart from './components/Recharts.jsx';
 
 function App() {
   return (
@@ -10,6 +14,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Login" element={<LoginPage />} />
+          <Route path="/example" element={<ChartApp />} />
+          <Route path="/example1" element={<Example width={500} height={500}/>} />
+          <Route path="/Rechart" element={<Rechart/>} />
+          <Route path="/example2" element={<Radar width={500} height={500} />} />
         </Routes>
       </Router>
     </>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,17 @@
-import React, {useState} from 'react';
-import TextField from '@mui/material/TextField';
+import React from 'react';
+import { Chart } from "react-google-charts";
 
-// learning events
-// learning states
-// learning hooks
 export const Home = () => {
-    const handleUpClick = () => {
-        console.log("Uppercase was clicked")
-        setText("You have clicked on button")
-    }
 
-    const handleOnChange = (event) => {
-        console.log("On change")
-        setText(event.target.value)
-    }
-
-    const [text, setText] = useState('None');
     return (
         <>
-        <div style={{ display: 'grid', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <TextField
-            id="outlined-multiline-static"
-            label="Multiline"
-            multiline
-            rows={4}
-            value={text}
-            onChange={handleOnChange}
+            <Chart
+            chartType="ScatterChart"
+            data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
+            width="100%"
+            height="400px"
+            legendToggle
             />
-        </div>
-
-        <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to Uppercase</button>
-
         </>
     );
 };
