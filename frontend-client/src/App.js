aff4804b68f65404841079f5558fd35bbf10c49d
cyber-shah <pranchx@gmail.com>
2023-11-13 20:27:16 -0500

    stable

---
+++ frontend-client/src/App.js
@@ -0,0 +1,17 @@
+// App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './Components/LoginPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
