d92c486cedbfc7e97e26d7aecf303f7a96288ae9
cyber-shah <pranchx@gmail.com>
2023-11-13 20:42:19 -0500

    login

--- frontend-client/src/Components/LoginPage.js
+++ frontend-client/src/Components/LoginPage.js
@@ -1,14 +1,18 @@
-import { Box, Form, FormField, Button } from 'grommet';
+import React from 'react';
+import { Box, Form, FormField, Button } from 'grommet'; // Import Grommet components
 
 function LoginPage() {
+  const styles = {
+  };
+
   return (
-      <Box width="medium" margin="auto">
-        <Form>
-          <FormField label="Username" name="username" />
-          <FormField label="Password" name="password" type="password" />
-          <Button type="submit" primary label="Submit" />
-        </Form>
-      </Box>
+    <Box width="medium" margin="auto" pad="large" height="large" justify='center'>
+      <Form >
+        <FormField label="Username" name="username" />
+        <FormField label="Password" name="password" type="password" />
+        <Button type="submit" primary label="Submit" />
+      </Form>
+    </Box>
   );
 }
 
