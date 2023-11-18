9582c300e93af84f83a417235803d328eb984fa3
cyber-shah <pranchx@gmail.com>
2023-11-17 22:51:11 -0500

    player home

--- frontend-client/src/App.js
+++ frontend-client/src/App.js
@@ -1,9 +1,10 @@
 // App.js
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './Components/LoginPage';
-import {DataTableExample} from './Components/Home';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from "./Components/LoginPage";
+import { DataTableExample } from "./Components/Home";
+import PlayerHome from "./Components/PlayerHome/PlayerHome";
 
 function App() {
   return (
@@ -11,6 +12,11 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<DataTableExample />} />
+        <Route
+          path="/PlayerHome"
+          // pass the player stats here
+          element={<PlayerHome stats={"../../jsons/career_stats.json"} />}
+        />
       </Routes>
     </Router>
   );
--- nbaApi.py
+++ nbaApi.py
@@ -2,6 +2,7 @@ import json
 import nba_api
 from nba_api.stats.endpoints import playercareerstats
 from nba_api.stats.static import players
+from nba_api.stats.static import teams
 
 # Create a PlayerCareerStats object
 career = playercareerstats.PlayerCareerStats(977)
@@ -15,9 +16,9 @@ with open('career_stats.json', 'w') as f:
 
 # get all players
 players = players.get_players()
-print(players)
-
 with open('all_players.json', 'w') as f:
     json.dump(players, f)
 
-
+teams = teams.get_teams()
+with open('all_teams.json', 'w') as f:
+    json.dump(teams, f)
