e386d4508aeb6753bc2d3f4110b51c05f08f6fdb
cyber-shah <pranchx@gmail.com>
2023-11-20 21:00:08 -0500

    more on this

--- frontend-client/src/Components/LoginPage.js
+++ frontend-client/src/Components/LoginPage.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { Box, Form, FormField, Button } from "grommet"; // Import Grommet components
+import { Box, Form, FormField, Button, Text } from "grommet"; // Import Grommet components
 import { useState } from "react";
 import { login } from "../api";
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
   const navigate = useNavigate();
@@ -24,50 +23,53 @@ function LoginPage() {
   const handleOnClick = async () => {
     const apiOutput = await login(username, password);
     console.log(apiOutput.message);
-    
+
     // Check the message and redirect accordingly
-    if (apiOutput.message === 'Login successful') {
-        // Redirect to the success page
-        navigate('/Home');
+    if (apiOutput.message === "Login successful") {
+      // Redirect to the success page
+      navigate("/Home");
+    } else {
+      // Redirect to the login page with an error message
+      navigate("/login", { errorMessage: apiOutput.message });
     }
-    else {
-        // Redirect to the login page with an error message
-        navigate('/login', { errorMessage: apiOutput.message });
-      }
   };
 
   return (
-    <Box
-      gap="large"
-      width="medium"
-      margin="auto"
-      pad="large"
-      height="large"
-      justify="center"
-    >
-      <Form>
-        <FormField
-          label="Username"
-          name="username"
-          onChange={handleChangeUsername}
-          id="username"
-          placeholder="mySQL username"
-        />
-        <FormField
-          label="Password"
-          placeholder="Password"
-          onChange={handleChangePassword}
-          name="password"
-          type="password"
-        />
-        <Button
-          type="submit"
-          primary
-          label="Login"
-          onClick={handleOnClick}
-        />
-      </Form>
-    </Box>
+    <div>
+      <Box
+        gap="large"
+        width="large"
+        margin="auto"
+        pad="large"
+        height="large"
+        justify="center"
+      >
+        <Form>
+          <FormField
+            label="Username"
+            name="username"
+            onChange={handleChangeUsername}
+            id="username"
+            placeholder="mySQL username"
+          />
+          <FormField
+            label="Password"
+            placeholder="Password"
+            onChange={handleChangePassword}
+            name="password"
+            type="password"
+          />
+          <Button type="submit" primary label="Login" onClick={handleOnClick} />
+        </Form>
+      </Box>
+
+
+      <Text size="medium" margin="auto" color="placeholder">
+        Please enter the User and password for mySQL database, it connects to
+        the database stored locally called - NBADatabase
+      </Text>
+
+    </div>
   );
 }
 
