e348acce4b58bfb1dd6c98c296b5ffb49b0f0884
cyber-shah <pranchx@gmail.com>
2023-11-21 00:17:25 -0500

    news

---
+++ frontend-client/src/Api/LeagueData/Route.js
@@ -0,0 +1,16 @@
+export async function GET() {
+    const nbaNewsResponse = await fetch(
+    "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/news?limit=50",
+    {
+      cache: "no-store",
+    }
+    );
+    
+  const nbaNews = await nbaNewsResponse.json();
+  
+    return {
+        LeagueNews: nbaNews
+    }
+}
+
+export default GET;
--- frontend-client/src/Components/Home.js
+++ frontend-client/src/Components/Home.js
@@ -1,23 +1,38 @@
 import React from "react";
 import { Box, Grid, Image, Text } from "grommet";
 import NewsCard from "./NewsCard";
+import GET from "../Api/LeagueData/Route";
 
-export default function Home() {
+export default function Home(props) {
+  const data = GET();
+
   return (
     <div>
       <Box style={{ height: "100vh", maxWidth: "1200px", margin: "auto" }}>
         <Grid columns={["3/4", "1/4"]} gap="xsmall">
           <Box pad="medium"></Box>
 
-          <Box pad="medium">
-            <Text >
-            News
-            </Text>
-            <NewsCard
-              imageUrl="https://example.com/image.jpg"
-              title="Card Title"
-              description="This is a description for the card."
-            />
+          {/* News side bar */}
+          <Box pad="small" >
+
+            <Box pad="small" border="all">
+              <Text >Media</Text>
+              <NewsCard
+                imageUrl="https://example.com/image.jpg"
+                title="Card Title"
+                description="This is a description for the card."
+              />
+            </Box>
+
+            <Box pad="small" border="all">
+              <Text>Recap</Text>
+              <NewsCard
+                imageUrl="https://example.com/image.jpg"
+                title="Card Title"
+                description="This is a description for the card."
+              />
+            </Box>
+            
           </Box>
         </Grid>
       </Box>
