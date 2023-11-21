81e2029214bbc6aa22b7a1f0bb8669690a511142
cyber-shah <pranchx@gmail.com>
2023-11-21 00:45:43 -0500

    news work

--- frontend-client/src/Api/LeagueData/Route.js
+++ frontend-client/src/Api/LeagueData/Route.js
@@ -1,6 +1,6 @@
 export async function GET() {
     const nbaNewsResponse = await fetch(
-    "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/news?limit=50",
+    "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/news?limit=10",
     {
       cache: "no-store",
     }
--- frontend-client/src/Components/Home.js
+++ frontend-client/src/Components/Home.js
@@ -1,20 +1,21 @@
-import {React, useState, useEffect} from "react";
+import { React, useState, useEffect } from "react";
 import { Box, Grid, Image, Text } from "grommet";
 import NewsCard from "./NewsCard";
 import GET from "../Api/LeagueData/Route";
 
 export default function Home(props) {
-    const [data, setData] = useState(null);
+  // data initally set to null
+  const [data, setData] = useState(null);
 
+  // TODO : learn more here
   useEffect(() => {
     const fetchData = async () => {
       const result = await GET();
       setData(result);
     };
-    fetchData();},
-    []);
-  console.log(data);
-  
+    fetchData();
+  }, []);
+
   return (
     <div>
       <Box style={{ height: "100vh", maxWidth: "1200px", margin: "auto" }}>
@@ -22,22 +23,23 @@ export default function Home(props) {
           <Box pad="medium"></Box>
 
           {/* News side bar */}
-            <Box pad="small" >
-              {data &&
-                data.LeagueNews &&
-                data.LeagueNews.articles &&
-                data.LeagueNews.articles.length > 0 &&
-                data.LeagueNews.articles.map((article, index) => (
-                  <NewsCard
-                    key={index}
-                    imageUrl={article.images[0].url}
-                    title={article.headline}
-                    description={article.description}
-                    publishedDate={new Date(article.published).toLocaleString()}
-                    linkUrl = {article.links.web.href}
-                  />
-                ))}
-            </Box>
+          <Box pad="small">
+            {/* TODO : learn more here */}
+            {data &&
+              data.LeagueNews &&
+              data.LeagueNews.articles &&
+              data.LeagueNews.articles.length > 0 &&
+              data.LeagueNews.articles.map((article, index) => (
+                <NewsCard
+                  key={index}
+                  imageUrl={article.images[0].url}
+                  title={article.headline}
+                  description={article.description}
+                  publishedDate={new Date(article.published).toLocaleString()}
+                  linkUrl={article.links.web.href}
+                />
+              ))}
+          </Box>
         </Grid>
       </Box>
     </div>
