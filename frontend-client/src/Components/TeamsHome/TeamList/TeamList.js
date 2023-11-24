c5a4ab4b13e7a2b9b31ca0dcd87382062279d848
cyber-shah <pranchx@gmail.com>
2023-11-24 09:11:53 -0500

    teams page

--- frontend-client/src/Components/TeamsHome/TeamList/TeamList.js
+++ frontend-client/src/Components/TeamsHome/TeamList/TeamList.js
@@ -1,18 +1,35 @@
-import React from 'react';
-import { Box, Text } from 'grommet';
+import React from "react";
+import { Box, Text, Anchor, Image } from "grommet";
 
 export default function TeamList(props) {
-    const league = props.teamData.sports[0].leagues[0];
-  return (
-      <div>
-          <Box>
-              <Text size='large'>
-                  {console.log(league)}  
-                  {league.name} Teams <br />
-                  {league.season.year} Year
-              </Text>
+  const league = props.teamData.sports[0].leagues[0];
+  const teamLinks = league.teams[0].team.links;
 
-          </Box>
+  return (
+    <div>
+      <Box>
+        <Text size="large">
+          {console.log(league)}
+          {league.name} Teams <br />
+          {league.season.year} Year
+        </Text>
+
+        <Text size="medium">{league.teams[0].team.displayName}</Text>
+
+        <Image
+          src={league.teams[0].team.logos[0].href}
+          width="100"
+          height="100"
+        />
+        
+              {/* Links */}
+        {teamLinks.map((link, index) => (
+          <Anchor key={index} size="xsmall" href={link.href}>
+            {link.text}
+          </Anchor>
+        ))}
+              
+      </Box>
     </div>
-  )
+  );
 }
