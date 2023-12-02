bdeefe70458f7392cf4da578fb4ca68b33609eb8
cyber-shah <pranchx@gmail.com>
2023-12-01 22:23:38 -0500

    team home looks fine

--- frontend-client/src/Components/TeamHome/TeamHeader.js
+++ frontend-client/src/Components/TeamHome/TeamHeader.js
@@ -1,14 +1,37 @@
 import React from "react";
 import { getTeamLogoUrl } from "../../API/EspnAPI/GetLogos";
-import { Image, Box } from "grommet";
+import { Image, Grid, Box, Text, Tabs, Tab } from "grommet";
 
 export default function TeamHeader(props) {
-  props.team.id = "1610612744";
+  const teamID = props.team.resultSets[0].rowSet[0][0];
+  const teamName = props.team.resultSets[0].rowSet[0][4];
+
   return (
-    <div>
-      <Box>
-        <Image src={getTeamLogoUrl(props.team.id)} width="150" height="150" />
-      </Box>
-    </div>
+    <Box
+      style={{ maxWidth: "500px", margin: "auto" }}
+      align="start"
+      pad="small"
+      justify="between"
+    >
+      <Grid columns={["150px", "flex"]} gap="small">
+        <Box>
+          <Image src={getTeamLogoUrl(teamID)} width="150" height="150" />
+        </Box>
+        <Box justify="center">
+          <Text size="xlarge">{teamName}</Text>
+        </Box>
+      </Grid>
+
+      <Tabs
+        activeIndex={props.selectedTab}
+        onActive={props.onActive}
+        alignControls="start"
+        margin="small"
+      >
+        <Tab title="Roster"></Tab>
+        <Tab title="Stats"></Tab>
+        <Tab title="Schedule"></Tab>
+      </Tabs>
+    </Box>
   );
 }
--- frontend-client/src/Components/TeamHome/TeamHome.js
+++ frontend-client/src/Components/TeamHome/TeamHome.js
@@ -1,39 +1,51 @@
 import React from "react";
 import { Data, DataTable, Toolbar, DataTableColumns } from "grommet";
 import TeamHeader from "./TeamHeader";
+import { useState } from "react";
 import { getTeamDetails } from "../../API/MySQL/TeamAPI";
 
-
 export default function TeamHome(props) {
-
-  const [teamDetails, setTeamDetails] = React.useState({});
+  const [teamDetails, setTeamDetails] = React.useState(null);
   const [teamSchedule, setTeamSchedule] = React.useState({});
   const [teamRoster, setTeamRoster] = React.useState({});
   const [teamStats, setTeamStats] = React.useState({});
 
-
+  // tabs --------------------------------------------------------------------
+  const [selectedTab, setTab] = useState(1);
+  const onActive = (index) => setTab(index);
+  // tabs --------------------------------------------------------------------
 
   React.useEffect(() => {
     async function fetchData() {
       try {
         // TODO : pass the team id and season from the props
-        const teamData = await getTeamDetails(2024, 1610612744);
-        setTeamDetails(teamData);
-        setTeamSchedule(teamData.teamSchedule);
-        setTeamRoster(teamData.teamRoster);
-        setTeamStats(teamData.teamStats);
+        const teamDetails = await getTeamDetails(2024, 1610612744);
+        setTeamDetails(teamDetails);
+
+        setTeamSchedule(teamSchedule);
+        setTeamRoster(teamRoster);
+        setTeamStats(teamStats);
       } catch (error) {
         console.error(error);
       }
     }
     fetchData();
-  } ,[]);
-  
+  }, []);
 
   return (
     <div>
-      {teamDetails != null && <TeamHeader team={teamDetails} />}
-      {teamDetails != null && console.log(teamDetails)}
+      {teamDetails != null && (
+        <TeamHeader
+          team={teamDetails}
+          selectedTab={selectedTab}
+          onActive={onActive}
+          setTab={setTab}
+        />
+      )}
+
+      {teamRoster !== null && selectedTab === 0 && GameContent()}
+      {teamStats !== null && selectedTab === 1 && TeamContent()}
+      {teamSchedule !== null && selectedTab === 2 && StandingsContent()}
     </div>
   );
 }
