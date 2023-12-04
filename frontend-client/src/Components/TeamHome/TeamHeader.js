cd10bebcdc104eb2b682cb217bc5bbd75696ab8d
cyber-shah <pranchx@gmail.com>
2023-12-03 23:12:45 -0500

    navigates to players

--- frontend-client/src/Components/TeamHome/TeamHeader.js
+++ frontend-client/src/Components/TeamHome/TeamHeader.js
@@ -1,46 +1,71 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { getTeamLogoUrl } from "../../API/EspnAPI/GetLogos";
 import { Image, Grid, Box, Text, Tabs, Tab, Select } from "grommet";
+import { getTeamDetails } from "../../API/MySQL/TeamAPI";
 
+/**
+ * @param {*} props TeamID: the team id of the team that teamHome wants to display
+ * selectedTab, * onActive, * setTab
+ * @returns a header component that contains the team logo, team name, and tabs
+ */
 export default function TeamHeader(props) {
-  const teamID = props.team.resultSets[0].rowSet[0][0];
-  const teamName = props.team.resultSets[0].rowSet[0][4];
+  const [teamDetails, setTeamDetails] = React.useState(null);
 
-  return (
-    <Box elevation="large" justify="end" align="start">
-      <Box
-        style={{ maxWidth: "1500px", margin: "auto" }}
-        align="center"
-        pad="small"
-        justify="between"
-      >
-        <Grid columns={["150px", "flex"]} gap="small">
-          {/* First half IMAGE */}
-          <Box>
-            <Image src={getTeamLogoUrl(teamID)} width="150" height="150" />
-          </Box>
-          {/* Second half team name */}
-          <Box justify="center">
-            <Text size="xlarge">{teamName}</Text>
-          </Box>
-        </Grid>
+  async function fetchData() {
+    try {
+      // TODO : pass the team id and season from the props
+      const teamDetails = await getTeamDetails(2024, props.id);
+      setTeamDetails(teamDetails);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-        {/* Tabs */}
-        <Box direction="row" justify="center" gap="small">
-          <Tabs
-            activeIndex={props.selectedTab}
-            onActive={props.onActive}
-            alignControls="center"
-            margin="auto"
-          >
-            <Tab title="Home"></Tab>
-            <Tab title="Roster"></Tab>
-            <Tab title="Stats"></Tab>
-            <Tab title="Schedule"></Tab>
-          </Tabs>
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-          {/* push this to the extreme right but horizontally in the same row as tabs */}
-          {/* <Box direction="row" justify="end" gap="small">
+  // TODO: check if this is correct way to do it
+  if (teamDetails !== null) {
+    console.log(teamDetails);
+    const teamID = teamDetails.resultSets[0].rowSet[0][0];
+    const teamName = teamDetails.resultSets[0].rowSet[0][4];
+
+    return (
+      <Box elevation="large" justify="end" align="start">
+        <Box
+          style={{ maxWidth: "1500px", margin: "auto" }}
+          align="center"
+          pad="small"
+          justify="between"
+        >
+          <Grid columns={["150px", "flex"]} gap="small">
+            {/* First half IMAGE */}
+            <Box>
+              <Image src={getTeamLogoUrl(teamID)} width="150" height="150" />
+            </Box>
+            {/* Second half team name */}
+            <Box justify="center">
+              <Text size="xlarge">{teamName}</Text>
+            </Box>
+          </Grid>
+
+          {/* Tabs */}
+          <Box direction="row" justify="center" gap="small">
+            <Tabs
+              activeIndex={props.selectedTab}
+              onActive={props.onActive}
+              alignControls="center"
+              margin="auto"
+            >
+              <Tab title="Home"></Tab>
+              <Tab title="Roster"></Tab>
+              <Tab title="Stats"></Tab>
+              <Tab title="Schedule"></Tab>
+            </Tabs>
+
+            {/* push this to the extreme right but horizontally in the same row as tabs */}
+            {/* <Box direction="row" justify="end" gap="small">
           <Select
             size="xsmall"
             options={["2023", "2022", "2021"]}
@@ -48,8 +73,9 @@ export default function TeamHeader(props) {
             onChange={({ seasonYear }) => props.setYear(seasonYear)}
           />
       </Box> */}
+          </Box>
         </Box>
       </Box>
-    </Box>
-  );
+    );
+  }
 }
--- frontend-client/src/Components/TeamHome/TeamHome.js
+++ frontend-client/src/Components/TeamHome/TeamHome.js
@@ -8,49 +8,41 @@ import TeamStats from "./TeamStats/TeamStats";
 import TeamRoster from "./TeamRoster/TeamRoster";
 import { useParams } from "react-router-dom";
 
+/**
+ * 
+ * @param {*} No Props needed, extracts teamId from the url and passes it into the all the components
+ * 1. ID is passed into the TeamHeader component to display the team logo and name
+ * 2. Season and ID is passed into the TeamRoster component to display the team roster
+ * 3. Season and ID is passed into the TeamStats component to display the team stats
+ * 4. Season and ID is passed into the TeamSchedule component to display the team schedule
+ * @returns 
+ */
 export default function TeamHome(props) {
   const { teamId } = useParams();
   console.log(teamId);
-
-  // TODO: set this inside the TeamHeader component
-  const [teamDetails, setTeamDetails] = React.useState(null);
 
   // tabs --------------------------------------------------------------------
   const [selectedTab, setTab] = useState(1);
   const onActive = (index) => setTab(index);
   // tabs --------------------------------------------------------------------
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        // TODO : pass the team id and season from the props
-        const teamDetails = await getTeamDetails(2024, 1610612744);
-        setTeamDetails(teamDetails);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    fetchData();
-  }, []);
-
   return (
     <div>
       {/* TODO: extract season year from this to be able to pass it into props */}
-      {teamDetails != null && (
+      {
         <TeamHeader
-          team={teamDetails}
+          teamID ={teamId}
           selectedTab={selectedTab}
           onActive={onActive}
           setTab={setTab}
         />
-      )}
+      }
 
       <Box width="1500px" justify="center" margin="auto" pad="medium">
         {selectedTab === 1 && <TeamRoster teamId={teamId} season={2024} />}
         {selectedTab === 2 && <TeamStats />}
         {selectedTab === 3 && <TeamSchedule />}
       </Box>
-
     </div>
   );
 }
