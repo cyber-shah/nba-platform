a6256546de4fe44bb1e5ceaed692c9eb4a672c5d
whuang3607 <floridian567@gmail.com>
2023-12-06 21:53:36 -0500

    added a updated version of database + python script

--- team_roster_for_Adi.py
+++ team_roster_for_Adi.py
@@ -31,14 +31,13 @@ for i in range(y, 2025):
     x += 1
     y += 1
 
-start_team_index = 0
-end_team_index = 1
+start_team_index = 6
+end_team_index = 7
 player_team_season = ""
 head_coach_season = ""
 assistant_coach_season = ""
 folder_path = "C:/Users/19782/Desktop/NE_ALIGN_Program/Second_Year_Coursework/CS_5200/Group_project_data/"
 
-#for each_team_id in teams:
 for each_team_id_index in range(start_team_index, end_team_index):
     for each_season in season_list:
         team_roster = commonteamroster.CommonTeamRoster(team_ids[each_team_id_index], each_season)
@@ -48,28 +47,35 @@ for each_team_id_index in range(start_team_index, end_team_index):
             current_player_team_season = "(" + str(current_player_id) + ",\'" + each_season + "\'," + str(team_ids[each_team_id_index]) + "),"
             if (current_player_id in player_ids) and (current_player_team_season not in player_team_season):
                 player_team_season += current_player_team_season
+            else:
+                print("Ghost player: " + str(current_player_id))
+        '''
         coaches_dict = team_roster.get_dict()['resultSets'][1]['rowSet']
         for each_coach in coaches_dict:
             if each_coach[7] == "Head Coach":
                 head_coach_id = each_coach[2]
                 head_coach_first_name = each_coach[3]
                 head_coach_last_name = each_coach[4]
+                # check for ' in both names in GM and Owner!!
                 current_head_coach_season = "(" + str(head_coach_id) + ",\'" + head_coach_first_name + "\',\'" + head_coach_last_name + "\'," + str(team_ids[each_team_id_index]) + ",\'" + each_season + "\'),"
                 if current_head_coach_season not in head_coach_season:
                     head_coach_season += current_head_coach_season
             elif each_coach[7] == "Assistant Coach":
                 assistant_coach_id = each_coach[2]
                 assistant_coach_first_name = each_coach[3]
+                # check for ' in both names!!
                 assistant_coach_last_name = each_coach[4]
                 current_assistant_coach_season = "(" + str(assistant_coach_id) + ",\'" + assistant_coach_first_name + "\',\'" + assistant_coach_last_name + "\'," + str(team_ids[each_team_id_index]) + ",\'" + each_season + "\'),"
                 if current_assistant_coach_season not in assistant_coach_season:
                     assistant_coach_season += current_assistant_coach_season
+        '''
         time.sleep(0.600)
 
 player_season_team_file_name = folder_path + "/player_season_team.txt"
 with open(player_season_team_file_name, 'w') as f:
     f.write(player_team_season)
 
+'''
 head_coach_season_file_name = folder_path + "/head_coach.txt"
 with open(head_coach_season_file_name, 'w') as f:
     f.write(head_coach_season)
@@ -77,6 +83,7 @@ with open(head_coach_season_file_name, 'w') as f:
 assistant_coach_season_file_name = folder_path + "/assistant_coach.txt"
 with open(assistant_coach_season_file_name, 'w') as f:
     f.write(assistant_coach_season)
+'''
 
 '''
 #test for 1 team in 1 season
