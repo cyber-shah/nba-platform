5e6aecbe650f0639706ac0d2f670be2d892f73ad
cyber-shah <pranchx@gmail.com>
2023-12-21 00:03:47 -0500

    All stable

--- frontend-client/src/Components/SeasonHome/Standings/StandingsTable.js
+++ frontend-client/src/Components/SeasonHome/Standings/StandingsTable.js
@@ -1,36 +1,55 @@
+
 import React from 'react';
-import { Table } from '@mantine/core';
+import { Table, Grid, Image } from '@mantine/core';
 
 export default function StandingsTable(props) {
   const entries = props.standings.children[0].standings.entries;
-  const stats = entries[0].stats;
+  const FirstStats = entries[2].stats;
 
-  const dataStats = stats.map((stat) => (
-    <Table.Th key={stat.shortDisplayName} style={{ fontSize: '10px', margin: '2px', padding: '2px' }}>
-      {stat.shortDisplayName}
-    </Table.Th>
-  ));
+  const dataStats = (
+    <Table.Tr>
+      <Table.Th key="Team">Team</Table.Th>
+      {FirstStats.map((stat) => (
+        <Table.Th key={stat.shortDisplayName}>{stat.shortDisplayName}</Table.Th>
+      ))}
+    </Table.Tr>
+  );
 
   const body = entries.map((entry) => (
     <Table.Tr key={entry.team.displayName}>
-      <Table.Td style={{ fontSize: '12px', margin: '2px', padding: '2px' }}>
-        {entry.team.displayName}
+
+      <Table.Td>
+        <Grid>
+          <Grid.Col span={8} >
+            <Image src={entry.team.logos[0].href} />
+
+          </Grid.Col>
+          <Grid.Col span={4}>
+            {entry.team.displayName}
+          </Grid.Col>
+        </Grid>
+
       </Table.Td>
-      {entry.stats.map((stat, index) => (
-        <Table.Td key={index} style={{ fontSize: '12px', margin: '2px', padding: '2px' }}>
-          {stat.displayValue}
-        </Table.Td>
-      ))}
-    </Table.Tr>
+      {
+        entry.stats.map((stat, index) => (
+          <Table.Td key={index}>{stat.displayValue}</Table.Td>
+        ))
+      }
+
+    </Table.Tr >
   ));
 
+
+
+
+
   return (
-    <Table
-      striped
-      highlightOnHover
-    >
-      <Table.Thead>{dataStats}</Table.Thead>
-      <Table.Tbody>{body}</Table.Tbody>
-    </Table>
+    <div style={{ overflowX: 'auto' }}>
+      <Table striped highlightOnHover>
+        <Table.Thead>{dataStats}</Table.Thead>
+        <Table.Tbody>{body}</Table.Tbody>
+      </Table>
+    </div>
   );
 }
+
